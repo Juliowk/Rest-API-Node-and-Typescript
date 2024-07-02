2afe59168e3984a8773deae6949891af4e71bcd8
Júlio Elias de Sousa rocha <julioeliasdesousarocha.work@gmail.com>
2024-07-02 09:42:24 -0300

    update

--- build/index.js
+++ build/index.js
@@ -1,6 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const knex_1 = require("./server/database/knex");
 const server_1 = require("./server/server");
-server_1.server.listen(process.env.PORT || 3333, () => {
-    console.log(`App rodando na porta ${process.env.PORT || 3333}`);
-});
+const startServer = () => {
+    server_1.server.listen(process.env.PORT || 3333, () => {
+        console.log(`App rodando na porta ${process.env.PORT || 3333}`);
+    });
+};
+if (process.env.IS_LOCALHOST !== 'true') {
+    knex_1.Knex.migrate.latest()
+        .then(() => {
+        startServer();
+    })
+        .catch((error) => console.log(error));
+}
+else {
+    startServer();
+}
